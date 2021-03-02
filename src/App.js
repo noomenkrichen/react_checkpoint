71723fd58b4bb70e880ffff5d82dca29d2b6f2c1
Noomen Krichen <nkrichen@outlook.com>
2021-03-02 12:35:58 +0100

    React.js checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  let name = "user name";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React JS Checkpoint</h1>
+      <h2>Registration Form for : {name}</h2>
+
+      <form>
+      <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="first-name" placeholder="First name..." />
+          <label for="first-name">First name</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="last-name" placeholder="Last name..." />
+          <label for="last-name">Last name</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="email" placeholder="name@example.com" />
+          <label for="email">Email address</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="password" class="form-control" id="password" placeholder="Password" />
+          <label for="password">Password</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="password" class="form-control" id="confirm-password" placeholder="Confirm Password" />
+          <label for="confirm-password">Confirm password</label>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Register</button>
+        
+      </form>
+  
     </div>
   );
 }
